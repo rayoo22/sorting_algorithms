415ec9127837f4f8f5f18efe719a6c60fa775592
rayoo22 <ryankibet14@gmail.com>
2023-02-21 05:37:32 -0800

    tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * bubble_sort - function to sort an array
+ * @array: array to sort
+ * @size of @array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+	int temp;
+
+	for (i = 0; i < length; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
